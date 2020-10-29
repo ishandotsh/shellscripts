fa0fbf30968d726ad86317e589d6eaefb0717ea4
Ishan <55034342+ishandotsh@users.noreply.github.com>
2020-10-29 22:26:18 +0530

    Add files via upload

---
+++ getnet.ps1
@@ -0,0 +1,17 @@
+﻿$networkname = "also dingus"
+
+$state = $(netsh wlan show interfaces | findstr "State") | Out-String
+$notconnected = $state.Contains("disconnected") -or $state.Contains("authenticating") `
+  -or $state.Contains("associating")
+
+while ($notconnected) {
+  netsh wlan connect $networkname
+  Start-Sleep -s 4
+  $state = $(netsh wlan show interfaces | findstr "State") | Out-String
+  if ($state.Contains("associating")) {
+    Start-Sleep -s 2
+  }
+  $state = $(netsh wlan show interfaces | findstr "State") | Out-String
+  $notconnected = $state.Contains("disconnected") -or $state.Contains("authenticating") `
+    -or $state.Contains("associating")
+}
